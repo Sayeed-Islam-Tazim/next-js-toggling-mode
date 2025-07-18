d3082ed6d1dca6bcc11bc3344638f5ba214f896e
Sayeed Islam Tazim <sayeeditt@gmail.com>
2025-07-18 12:42:17 +0600

    toggle icon improved

--- app/components/modeToggler.tsx
+++ app/components/modeToggler.tsx
@@ -12,10 +12,10 @@ const ModeToggler = () => {
       className="flex items-center gap-2 border-2 border-green-100 rounded-xl px-2 py-1 relative cursor-pointer"
       onClick={toggle}
     >
-      <div className="">L</div>
-      <div className="">D</div>
+      <div className="">🌚</div>
+      <div className="">🌞</div>
       <div
-        className={`bg-green-100 rounded-xl h-4 w-4 absolute ${
+        className={`bg-green-100 rounded-xl h-5 w-5 absolute ${
           mode === "dark" ? "right-2" : "left-2"
         }`}
       />
